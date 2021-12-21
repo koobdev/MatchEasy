8e9d9921521d38a99a50af58dfc161c1ba3101fa
koobdev <gupnodo21@gmail.com>
2021-12-22 00:17:22 +0900

    Create Project

---
+++ src/main/java/koo/project/matcheasy/MatcheasyApplication.java
@@ -0,0 +1,13 @@
+package koo.project.matcheasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatcheasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatcheasyApplication.class, args);
+	}
+
+}
